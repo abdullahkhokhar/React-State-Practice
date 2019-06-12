4cf7fd321a2f3000ce3b1fb0e08e54af75282642
Abdullah Khokhar <abdullah.khokhar@mail.utoronto.ca>
2019-06-12 11:07:38 -0400

    stylign added

--- coinflipper/src/Coin.js
+++ coinflipper/src/Coin.js
@@ -1,9 +1,12 @@
-improt React, {Component} from 'react'
+import React, {Component} from 'react';
+import "./Coin.css"
 
 class Coin extends Component{
   render(){
     return(
-      return <img src = {this.props.info.imgSrc} alt = {this.props.info.side}/>
+      <div className = "Coin">
+        <img src = {this.props.info.imgSrc} alt = {this.props.info.side}/>
+      </div>
     );
   }
 }
